2940d6e33579e68aaa045d154644c880314e499c
Prem Kumar <prembammidi25@msitprogram.net>
2020-07-07 15:24:36 +0530

    Google Login

--- google_fit/App.js
+++ google_fit/App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native';
+import { logger } from 'react-native-logs';
+import config from './config';
+import * as Google from 'expo-google-app-auth';
 
-export default function App() {
+class App extends Component {
+  
+  signIn = async() => {
+    var log = logger.createLogger();
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: config.Android_client_id,
+        iosClientId: config.IOS_client_secret
+      });
+
+      if (result.type === 'success') {
+        console.log(result.accessToken)
+      }
+    }
+    catch(e) {
+      console.log(e)
+      return {error: true}
+    }
+  }
+
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.welcome}>
+        Welcome to Google Fit Integration
+      </Text>
+      <View>
+        <TouchableOpacity style={styles.button} onPress={() => this.signIn()}>
+          <Text style={{ fontSize: 20, color:'white' }}>Click here to Sign In with Google</Text>
+        </TouchableOpacity>
+      </View>
+    </View> 
   );
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00a8b5',
+  },
+    welcome: {
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff',
+    margin: 10,
   },
 });
+
+
