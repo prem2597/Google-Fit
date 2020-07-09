5c91d38a2ebdc655a8933c0df001640d4d0f8996
Prem Kumar <prembammidi25@msitprogram.net>
2020-07-09 09:44:32 +0530

    Google fit page complete

--- google_fit/screens/GoogleFit.js
+++ google_fit/screens/GoogleFit.js
@@ -19,24 +19,24 @@ class GoogleFitData extends Component {
   }
 
   postData = async () => {
-    axios({
-      method: 'post',
-      url: 'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
-      headers: {
-        'Authorization': 'Bearer ' + this.state.access_token
-      }, 
-      data: {
-        "aggregateBy": [{
-          "dataTypeName": "com.google.step_count.delta",
-          "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
-        }],
-        "bucketByTime": { "durationMillis": 86400000 },
-        "startTimeMillis": 1594060200000,
-        "endTimeMillis": 1594146600000
-      }
-    }).then((resp) => {
-      console.log(resp.data)
-    });
+    // axios({
+    //   method: 'post',
+    //   url: 'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
+    //   headers: {
+    //     'Authorization': 'Bearer ' + this.state.access_token
+    //   }, 
+    //   data: {
+    //     "aggregateBy": [{
+    //       "dataTypeName": "com.google.step_count.delta",
+    //       "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+    //     }],
+    //     "bucketByTime": { "durationMillis": 86400000 },
+    //     "startTimeMillis": 1594060200000,
+    //     "endTimeMillis": 1594146600000
+    //   }
+    // }).then((resp) => {
+    //   console.log(resp.data)
+    // });
   }
 
   render() {
@@ -47,19 +47,23 @@ class GoogleFitData extends Component {
             </Text>
             <View>
               <View>
-                <Text style={styles.header}>Please select the kind of data you want to see from the below list</Text>
+                <Text style={styles.header}>You can see the below mentioned details in your Data sheet</Text>
+                <Text style={styles.header}>Please tap on click here button</Text>
               </View>
               <View>
 
               </View>
+              <View>
+                <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center', paddingTop: 55 }}>Step Count</Text>
+              </View>
+              <View>
+                <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center' }}>Physical Measurement</Text>
+              </View>
+              <View>
+                <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center' }}>Heart Rate</Text>
+              </View>
               <TouchableOpacity style={styles.button} onPress={() => this.postData()}>
-                  <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center', paddingTop: 55 }}>Step Count</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={() => this.postData()}>
-                  <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center' }}>Blood Pressure</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={() => this.postData()}>
-                  <Text style={{ fontSize: 20, color:'white', margin: 15, textAlign:'center' }}>Heart Rate</Text>
+                  <Text style={{ fontSize: 20, color:'red', margin: 15, textAlign:'center', padding: 55 }}>Click Here</Text>
               </TouchableOpacity>
             </View>
         </View> 
